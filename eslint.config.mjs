8fd6e36b57f1164af0db450d22efa4d0cd8a3a81
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-21 21:20:16 +0600

    start

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['node_modules', 'dist'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslintPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prettier/prettier': 'error', // Enforce Prettier formatting
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslintPlugin.configs.recommended.rules,
+    },
+  },
+];
