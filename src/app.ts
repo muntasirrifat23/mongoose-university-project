b244815c175df31218aea5c5a80d789d966509c0
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-12-01 14:00:36 +0600

    not found

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
-import express, {
-  Application,
-  Request,
-  Response,
-} from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/studen.route';
+import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/User/user.router';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 app.use(express.json());
@@ -21,7 +18,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send(a);
 });
 
-// Error-Handling Middleware
+// Error Handling Middleware
 app.use(globalErrorHandler);
+
+// Not Found
+app.use(notFound);
 
 export default app;
